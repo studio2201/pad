6e8d3d56182cdd4ad00a19bf8321854775712f74
jeryd <jerydleuck@gmail.com>
2026-06-25 13:48:28 -1000

    Use numeric UID:GID in dockerImage User config to avoid passwd file dependency

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
             ExposedPorts = {
               "4402/tcp" = {};
             };
-            User = "nobody:nobody";
+            User = "65534:65534";
           };
 
           # Create /app directory structure inside the container
